935f84f9b325f8e54a76f4a45cc3c88b331d2e48
Gabaghooul <samdoyle345@gmail.com>
2026-09-15 19:31:27 +0100

    Basic hit reaction for Enemy

--- enemy.gd
+++ enemy.gd
@@ -4,6 +4,7 @@ extends CharacterBody3D
 
 func take_damage(damage):
 	if hp > 0:
+		$AnimationPlayer.play("Hurt")
 		hp -= damage
 		print("Enemy hit: ", hp, "/", 100)
 	if hp <= 0:
--- weapon.gd
+++ weapon.gd
@@ -14,7 +14,7 @@ func finish_attack():
 	$VisualPivot/Hitbox.set_deferred("monitoring", false)
 
 func _on_hitbox_area_entered(area: Area3D) -> void:
-	var enemy = area.get_parent()
+	var enemy = area.get_parent().get_parent()
 	if enemy.has_method("take_damage") && !connected_hits.has(enemy):
 		enemy.take_damage(damage)
 		connected_hits.append(enemy)
